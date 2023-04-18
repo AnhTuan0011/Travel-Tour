13cc0fd4f1671d370da53c0610df6e0db45c77aa
LeAnhTuan647 <92526269+AnhTuan0011@users.noreply.github.com>
2023-04-18 17:41:29 +0700

    first commit

---
+++ MENU/menujs.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($) { 
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 10) {
+            $('.homelist').addClass('active');
+        }
+        else {
+            $('.homelist').removeClass('active');
+        }
+    })
+    })
+
+    // trung js vs FAQ (botton search)
+const btnSearch = document.getElementById("search")
+const boxSearch = document.getElementById("box-search")
+
+btnSearch.addEventListener("click", () => {
+    btnSearch.classList.toggle('start');
+    boxSearch.classList.toggle('start');
+})
