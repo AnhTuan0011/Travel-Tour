b2ab5ae315b910411a485ea12883c8324f6d737d
LeAnhTuan647 <92526269+AnhTuan0011@users.noreply.github.com>
2023-04-19 17:59:24 +0700

    4st

---
+++ JS/styletravel.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:40,
+    // nav:true,
+    items:3,
+    autoplay:true,
+    autoplayTimeout:6000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:3
+        }
+    }
+})
